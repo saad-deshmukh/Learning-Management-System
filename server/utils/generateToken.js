6e9dd0ad2c423a035f2549c9c21ce29068d8221e
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-05-19 10:57:01 +0530

    changes

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -10,6 +10,7 @@ export const generateToken = (res, user, message) => {
     .cookie("token", token, {
       httpOnly: true,
       sameSite: "none",
+      secure: true, 
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     }).json({
         success:true,
