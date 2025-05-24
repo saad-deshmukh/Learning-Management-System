9000a3c939bc266f835f8f339f9003ce343929a6
saad-deshmukh <deshmukhsaad13@gmail.com>
2025-05-24 18:14:34 +0530

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,7 +43,8 @@ const Navbar = () => {
   }, [isSuccess]);
 
   return (
-    <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
+    <div className="h-16 dark:bg-[#1E1B4B] bg-[#E0F2FE] border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
+
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
         <div className="flex items-center gap-2">
